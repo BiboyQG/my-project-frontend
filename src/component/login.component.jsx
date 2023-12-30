3db3f1220b2976f7cab250c6c74612ecb7134b30
Banghao Chi <banghao2@illinois.edu>
2023-12-30 11:09:37 +0800

    Added self-made input component

--- src/component/login.component.jsx
+++ src/component/login.component.jsx
@@ -1,32 +1,58 @@
-import { Button, Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import FormInput from "./form-input/form-input.component";
+import { useState } from "react";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-    return (
-      <div className="w-96 bg-white flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
-          <div className="text-4xl font-bold text-center text-gray-800">
-            Login
-          </div>
-          <div className="w-full px-8 py-4 mt-6 bg-gray-100 rounded-lg">
-            <div className="flex flex-col items-center justify-center">
-              <div className="w-full">
-                <Input type="email" label="Email" />
-              </div>
-              <div className="w-full mt-4">
-                <Input
-                  type="password"
-                  label="Password"
-                  className="drop-shadow-none"
-                />
-              </div>
-              <Button color="primary" variant="ghost" className="mt-4">
-                Ghost
-              </Button>
+
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="w-96 bg-white flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-4xl font-bold text-center text-gray-800">
+          Login
+        </div>
+        <div className="w-full px-8 py-4 mt-6 bg-gray-100 rounded-lg">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full">
+              <FormInput
+                label="Email"
+                type="email"
+                required
+                onChange={handleChange}
+                name="email"
+                value={email}
+              />
             </div>
+            <div className="w-full mt-4">
+              <FormInput
+                label="Password"
+                type="password"
+                required
+                onChange={handleChange}
+                name="password"
+                value={password}
+              />
+            </div>
+            <Button color="primary" variant="ghost" className="mt-4">
+              Ghost
+            </Button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
