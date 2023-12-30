867a702e17338762b2190dede7920177d30fa13a
Banghao Chi <banghao2@illinois.edu>
2023-12-30 11:34:44 +0800

    Installed Axios

--- src/component/login.component.jsx
+++ src/component/login.component.jsx
@@ -17,35 +17,49 @@ const Login = () => {
     setFormFields({ ...formFields, [name]: value });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+       
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <div className="w-96 bg-white flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center">
         <div className="text-4xl font-bold text-center text-gray-800">
           Login
         </div>
-        <div className="w-full px-8 py-4 mt-6 bg-gray-100 rounded-lg">
+        <div className="text-gray-500 mt-6 text-sm">
+          Sign in with your email and password
+        </div>
+        <div className="w-full px-8 py-4 mt-6 bg-gray-100 rounded-lg shadow-md">
           <div className="flex flex-col items-center justify-center">
-            <div className="w-full">
-              <FormInput
-                label="Email"
-                type="email"
-                required
-                onChange={handleChange}
-                name="email"
-                value={email}
-              />
-            </div>
-            <div className="w-full mt-4">
-              <FormInput
-                label="Password"
-                type="password"
-                required
-                onChange={handleChange}
-                name="password"
-                value={password}
-              />
-            </div>
-            <Button color="primary" variant="ghost" className="mt-4">
+            <form>
+              <div className="w-full">
+                <FormInput
+                  label="Email"
+                  type="email"
+                  required
+                  onChange={handleChange}
+                  name="email"
+                  value={email}
+                />
+              </div>
+              <div className="w-full mb-2">
+                <FormInput
+                  label="Password"
+                  type="password"
+                  required
+                  onChange={handleChange}
+                  name="password"
+                  value={password}
+                />
+              </div>
+            </form>
+            <Button color="primary" variant="solid" className="my-4">
               Ghost
             </Button>
           </div>
